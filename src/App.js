402445877a50ca28510715f19a880fb50b41e737
Italo Devoto Ramella <italodr@gmail.com>
2018-04-20 15:23:08 +0200

    Improve font perfomance, Add fontfaceobserver

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 import React, { Component } from 'react';
+import FontFaceObserver from 'fontfaceobserver';
 
 import Header from 'components/Header';
 import Footer from 'components/Footer';
 import Layout from 'components/Layout';
 
+import './lato.css';
+
 class App extends Component {
+  constructor() {
+    super();
+
+    Promise.all([new FontFaceObserver('Lato')])
+      .then(() => {
+        document.documentElement.classList.add('fonts-loaded');
+      })
+      .catch(err => {
+        // console.error('Failed to load fonts!', err);
+      });
+  }
+
   render() {
     return (
       <div>
